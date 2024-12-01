7cea896304b90be895ab122855f30f605bb7267a
Julian Soj <guidosoynozki@gmail.com>
2024-11-30 22:49:20 -0300

    Web1 Topics

--- index.ts
+++ index.ts
@@ -1,4 +1,7 @@
-import puppeteer, { Browser } from 'puppeteer';
+import { Browser, DEFAULT_INTERCEPT_RESOLUTION_PRIORITY  } from 'puppeteer';
+import puppeteer from 'puppeteer-extra';
+import AdblockerPlugin from 'puppeteer-extra-plugin-adblocker'
+
 import { link, writeFile } from 'fs';
 
 
@@ -6,6 +9,12 @@ const url = '';
 
 const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
 
+//https://www.npmjs.com/package/puppeteer-extra-plugin-adblocker
+puppeteer.use(
+  AdblockerPlugin({
+    interceptResolutionPriority: DEFAULT_INTERCEPT_RESOLUTION_PRIORITY
+  })
+)
 
 const main = async () => {
   const browser: Browser = await puppeteer.launch();
@@ -22,13 +31,16 @@ const main = async () => {
   });
   
   const articlesData = [];
+  const articlesTopics = [];
+
   for (const link of articleLinks) {
     await delay(1000);
     await page.goto(link);
     await delay(2000);
     
     await page.waitForSelector('main section article');
-
+    const articleTopics:{}[] = []
+    const articleUrlTopics:string[] = []
     const articleData = await page.evaluate(() => {
       //header
       const heading = document.querySelector('header').querySelector('h1').innerText
@@ -63,10 +75,29 @@ const main = async () => {
 
       const parser = new DOMParser();
       const doc = parser.parseFromString(article, 'text/html');
-      
-      const contentArray = Array.from(doc.querySelectorAll('p, img, blockquote, h2, h3'))
 
-      const articleFormat = (array:Element[])=>{
+      const anchorArray = Array.from(doc.querySelectorAll('a'))
+      const urlTopics = ()=>{
+        return anchorArray.map((element:HTMLAnchorElement)=>{
+          if(element.href.split('/')[3] == 'category'){
+            return element.innerText.trim().toLowerCase().split(' ').join('-')
+            
+          } 
+          else null
+        })
+      }
+      const topics = ()=>{
+        return anchorArray.map((element:HTMLAnchorElement)=>{
+          if(element.href.split('/')[3] == 'category'){
+            return {name:element.innerText.trim(),url:element.innerText.trim().toLowerCase().split(' ').join('-')}
+          } 
+          else null
+        })
+      }
+
+
+      const contentArray = Array.from(doc.querySelectorAll('p, img, blockquote, h2, h3, q'))
+      const articleFormatContent = (array:Element[])=>{
         return array.map((element:Element)=>{
           console.log(element)
           if (element.tagName === 'H2') {
@@ -85,18 +116,28 @@ const main = async () => {
           }
           
           
-          if(element.tagName === 'P')return {
+          if(element.tagName === 'P'){
+            const paragraphElement = element as HTMLParagraphElement;
+            if(paragraphElement.querySelector('span')?.innerText =='Topics') return null
+
+            else return {
             htmlParagraph:
               element.innerHTML
-          }
+            }
+        }
           
+          if(element.tagName === 'Q'){
+            const quoteElement = element as HTMLQuoteElement;
+          
+            return {
+            quote:quoteElement.innerText,
+          }}
           if(element.tagName === 'BLOCKQUOTE'){
             const quoteElement = element as HTMLQuoteElement;
             
             return {
             quote:quoteElement.innerText,
           }}
-
         })
       }
 
@@ -108,17 +149,20 @@ const main = async () => {
         frontImageAlt:frontImageAlt,
         heading:heading,
         subheading:subheading,
+        urlTopics:urlTopics(),
+        topics:topics(),
         date:`Timestamp.fromDate(new Date('${date}'))`,
-        content:articleFormat(contentArray)
+        content:articleFormatContent(contentArray)
        };
     });
-
+    articlesTopics.push(articleData.topics)
     articlesData.push({source:link, ...articleData });
   }
-
+  console.log(articlesTopics)
   console.log(articlesData);
-  const json = JSON.stringify(articlesData)
-  writeFile(`./export/...`, json, 'utf8', (err) => {
+
+  const json = JSON.stringify({articlesTopics,articlesData})
+  writeFile(`./export/...json`, json, 'utf8', (err) => {
     if (err) console.error(err)})
 
   await browser.close();
